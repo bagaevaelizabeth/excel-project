06f313a3be93aa0350cc79389d26fd12df332895
Elizabeth Baga <bagaevaelizabeth@gmail.com>
2021-01-23 01:31:49 +0300

    add selection of cells

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
-import {$} from '@core/dom';
-import {shouldResize} from '@/components/table/table.functions';
+import {isCell, shouldResize} from '@/components/table/table.functions';
 import {resizeHandler} from '@/components/table/table.resize';
+import {TableSelection} from '@/components/table/TableSelection';
+import {$} from '@core/dom';
+import {matrix} from '@core/utils';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -17,9 +19,30 @@ export class Table extends ExcelComponent {
     return createTable();
   }
 
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selection.select($cell);
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      if (event.shiftKey) {
+        const target = $target.id(true);
+        const current = this.selection.current.id(true);
+        const $cells = matrix(target, current).map(id => this.$root.find(`[data-id="${id}"]`));
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
 }
+
